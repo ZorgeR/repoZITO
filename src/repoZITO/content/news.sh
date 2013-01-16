6555415bd76890b3f127ccede1d03b106aeaad8b
ZorgeR <zorg.rhrd@gmail.com>
2013-01-16 23:50:00 +0400

    r648
    Signed-off-by: ZorgeR <zorg.rhrd@gmail.com>

--- src/repoZITO/content/news.sh
+++ src/repoZITO/content/news.sh
@@ -8,9 +8,10 @@
 # Copyright - Zorge.R - 2010 - motofan.ru
 #
 
+rm "$repoz_tmp/news.list"
 showNotify "repoZITO" "$rz_start_download" 1 1
-$rz_wget -O $repoz_tmp/news.list $rz_serv/news.list
-rz_news_txt=`cat $repoz_tmp/news.list`
+$rz_wget -O "$repoz_tmp/news.list" "$rz_serv/news.list"
+rz_news_txt=`cat "$repoz_tmp/news.list"`
 showQ "repoZITO" "$rz_news_txt" 2
 
 . $repoz_content/repoZITO.sh

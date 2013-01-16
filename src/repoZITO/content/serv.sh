58a6ca7bc14330aec32096159875ed158591acaa
ZorgeR <zorg.rhrd@gmail.com>
2013-01-16 23:44:10 +0400

    r621
    Signed-off-by: ZorgeR <zorg.rhrd@gmail.com>

--- src/repoZITO/content/serv.sh
+++ src/repoZITO/content/serv.sh
@@ -17,8 +17,7 @@ ret=$?
 
 case $ret in
 	1)	
-		rz_newserv=`showTextEntry "$repoz_content/reposerv" "repoZITO" "$rz_newserv_enter" 0`
-		if [ $rz_model = E2 ];then echo "$rz_newserv" > $repoz_content/reposerv;fi
+		if [ $rz_model = E2 ];then rz_newserv=`showTextEntry 0 "repoZITO" "Enter adress with http:// or ftp://"`;echo "$rz_newserv" > "$repoz_content/reposerv";else rz_newserv=`showTextEntry "$repoz_content/reposerv" "repoZITO" "$rz_newserv_enter" 0`;fi
 		rz_newserv_txt=`cat $repoz_content/reposerv`
 		showQ "$rz_newserv_compl" "$rz_newserv_compl_txt - $rz_newserv_txt" 2
 		;;
